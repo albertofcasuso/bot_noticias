32b18eaa78027c2c58b07efd3f9e9638cf56214c
albertofcasuso <albertofcasuso@gmail.com>
2019-10-19 12:45:26 -0500

    Crea un index.html que contiene el top de noticias de la newsAPI

--- bot_noticias.py
+++ bot_noticias.py
@@ -5,5 +5,23 @@ import secret
 newsapi = NewsApiClient(api_key=secret.api_key)
 
 # /v2/top-headlines
-top_headlines = newsapi.get_sources()
-print(top_headlines)
+top_headlines = newsapi.get_top_headlines(country='mx')
+
+lista = ""
+articulos = top_headlines['articles']
+for articulo in articulos:
+    # print(articulo['description'])
+    # print(articulo['urlToImage'])
+    lista += """
+    <div class="card mb-4">
+      <img class="card-img-top" src='"""+articulo['urlToImage']+"""' alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">"""+articulo['title']+"""</h2>
+        <p class="card-text"></p>
+        <a href='"""+articulo['url']+"""' class="btn btn-primary">Leer más &rarr;</a>
+      </div>
+      <div class="card-footer text-muted">
+        """+articulo['publishedAt']+"""
+      </div>
+    </div>
+    """
---
+++ index_generator.py
@@ -0,0 +1,145 @@
+import bot_noticias
+import datetime
+
+fecha = datetime.datetime.now()
+index = open('index.html', 'w')
+
+html = """
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Top noticias</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <!-- Blog Entries Column -->
+      <div class="col-md-8">
+
+        <h1 class="my-4">Últimas noticias
+        </h1>
+
+        <!-- Blog Post -->
+        """+bot_noticias.lista+"""
+
+      </div>
+
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4">
+
+        <!-- Categories Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">Categories</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">Web Design</a>
+                  </li>
+                  <li>
+                    <a href="#">HTML</a>
+                  </li>
+                  <li>
+                    <a href="#">Freebies</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="#">CSS</a>
+                  </li>
+                  <li>
+                    <a href="#">Tutorials</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Side Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">¿Qué es esto?</h5>
+          <div class="card-body">
+            Esto es un pequeño experimento en Python que recoge los titulares más importantes de las noticias en México.
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+"""
+index.write(html)
+index.close()
