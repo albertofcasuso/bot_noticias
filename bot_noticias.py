9b812e49208c46a6b589f55234642bb62ffa1410
albertofcasuso <albertofcasuso@gmail.com>
2019-10-22 10:08:28 -0500

    Los links se abren en una pestaña nueva

--- bot_noticias.py
+++ bot_noticias.py
@@ -10,16 +10,13 @@ top_headlines = newsapi.get_top_headlines(country='mx')
 lista = ""
 articulos = top_headlines['articles']
 for articulo in articulos:
-    # print(articulo['description'])
-    # print(articulo['urlToImage'])
-    # print(articulo['source']['name'])
     lista += """
     <div class="card mb-4">
       <img class="card-img-top" src='"""+str(articulo['urlToImage'])+"""' alt="Card image cap">
       <div class="card-body">
         <h2 class="card-title">"""+str(articulo['title'])+"""</h2>
         <p class="card-text"></p>
-        <a href='"""+str(articulo['url'])+"""' class="btn btn-primary">Leer más &rarr;</a>
+        <a href='"""+str(articulo['url'])+"""' class="btn btn-primary" target="_blank" and rel="noopener noreferrer">Leer más &rarr;</a>
       </div>
       <div class="card-footer text-muted">
         """+str(articulo['source']['name'])+"""
