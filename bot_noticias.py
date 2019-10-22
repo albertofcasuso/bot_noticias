658a4932efb7006f741d01e321ffdb3cdb82de1b
albertofcasuso <albertofcasuso@gmail.com>
2019-10-22 15:35:35 -0500

    Elimina los "- nombredeperiodico" del título de las noticias. También elimina el específico del universal.

--- bot_noticias.py
+++ bot_noticias.py
@@ -8,13 +8,20 @@ newsapi = NewsApiClient(api_key=secret.api_key)
 top_headlines = newsapi.get_top_headlines(country='mx')
 
 lista = ""
+separador = ' - '
+separador_el_universal = ' | '
 articulos = top_headlines['articles']
 for articulo in articulos:
+    if separador_el_universal in articulo['title']:
+        titulo = articulo['title'].split(separador_el_universal, 1)[0]
+    else:
+        titulo = articulo['title'].split(separador, 1)[0]
+
     lista += """
     <div class="card mb-4">
       <img class="card-img-top" src='"""+str(articulo['urlToImage'])+"""' alt="Card image cap">
       <div class="card-body">
-        <h2 class="card-title">"""+str(articulo['title'])+"""</h2>
+        <h2 class="card-title">"""+titulo+"""</h2>
         <p class="card-text"></p>
         <a href='"""+str(articulo['url'])+"""' class="btn btn-primary" target="_blank" and rel="noopener noreferrer">Leer más &rarr;</a>
       </div>
